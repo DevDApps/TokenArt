36472d5f3779af13e80fef7985a0331f5f4fe4ba
susan <susannajbrown@yahoo.com>
2021-03-05 15:54:03 -0500

    Updates

---
+++ precache-manifest.f687ceed99c5f7e09ec8baff6e98ea85.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "190aa1b41cf451dd18ff744cb2a81431",
+    "url": "/EdCert/index.html"
+  },
+  {
+    "revision": "cf11cdc0f76f72b39b8a",
+    "url": "/EdCert/static/css/2.80dce9aa.chunk.css"
+  },
+  {
+    "revision": "be75389ba6a44cb2334b",
+    "url": "/EdCert/static/css/main.18de8aef.chunk.css"
+  },
+  {
+    "revision": "cf11cdc0f76f72b39b8a",
+    "url": "/EdCert/static/js/2.fd9417dd.chunk.js"
+  },
+  {
+    "revision": "30545a24812bc2557c4d652dede4cf2a",
+    "url": "/EdCert/static/js/2.fd9417dd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "be75389ba6a44cb2334b",
+    "url": "/EdCert/static/js/main.1c447867.chunk.js"
+  },
+  {
+    "revision": "ec6a6a237446ca43952a",
+    "url": "/EdCert/static/js/runtime-main.d9b28bd2.js"
+  },
+  {
+    "revision": "23bc952edc513fcb635918fd3985d0f4",
+    "url": "/EdCert/static/media/photo.23bc952e.png"
+  }
+]);
