fb16d080488dceb81244ab5de028008ac30a1890
susan <susannajbrown@yahoo.com>
2021-03-14 21:32:29 -0400

    Updates

---
+++ precache-manifest.8cfb98c597b0438d534e7f93ae5d3ff0.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "37a977edcde7b05236f154cd360d86c4",
+    "url": "/TokenArt/index.html"
+  },
+  {
+    "revision": "faa53506b4752db7b4e6",
+    "url": "/TokenArt/static/css/2.80dce9aa.chunk.css"
+  },
+  {
+    "revision": "a2ced7f79a3eb8b59e2d",
+    "url": "/TokenArt/static/css/main.18de8aef.chunk.css"
+  },
+  {
+    "revision": "faa53506b4752db7b4e6",
+    "url": "/TokenArt/static/js/2.2e5a3f25.chunk.js"
+  },
+  {
+    "revision": "30545a24812bc2557c4d652dede4cf2a",
+    "url": "/TokenArt/static/js/2.2e5a3f25.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a2ced7f79a3eb8b59e2d",
+    "url": "/TokenArt/static/js/main.1024093a.chunk.js"
+  },
+  {
+    "revision": "bcc8a210930e6dc010e9",
+    "url": "/TokenArt/static/js/runtime-main.8c522e6b.js"
+  },
+  {
+    "revision": "3912faa30c9cbe24519220ca5eab4d40",
+    "url": "/TokenArt/static/media/logo.3912faa3.png"
+  }
+]);
